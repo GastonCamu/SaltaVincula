9a42cb55e893ad240ead60bc7cf0275e2542aeda
Gastón Camú <camugaston2002@gmail.com>
2023-11-16 00:02:47 -0300

    se modifico tab3

--- SaltaVincula/src/app/tab3/tab3.page.ts
+++ SaltaVincula/src/app/tab3/tab3.page.ts
@@ -10,6 +10,13 @@ export class Tab3Page {
   seguidores: number = 720;
   siguiendo: number = 1960;
 
+  header_3_text_nombre: string = "Gemus gabian";
+  header_3_text_edad: string = "30 años";
+  header_3_text_lugar: string = "Salta Capital";
+  header_3_text_oficio: string = "Carpintero";
+
+  historias = ["navidad", "vacaciones", "nuevo perrito", "primer trabajo", "navidad", "vacaciones", "nuevo perrito", "primer trabajo"];
+
   constructor() {}
 
 }
