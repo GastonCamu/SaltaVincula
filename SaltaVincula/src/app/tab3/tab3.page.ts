2003a38deab240f8783015a224f9a2f5bc07626c
Gastón Camú <camugaston2002@gmail.com>
2023-11-18 09:25:50 -0300

    se hizo un diseño mas responsive de perfil y se agrego una galeria de imgs

--- SaltaVincula/src/app/tab3/tab3.page.ts
+++ SaltaVincula/src/app/tab3/tab3.page.ts
@@ -10,10 +10,10 @@ export class Tab3Page {
   seguidores: number = 720;
   siguiendo: number = 1960;
 
-  header_3_text_nombre: string = "Gemus gabian";
-  header_3_text_edad: string = "30 años";
+  header_3_text_nombre: string = "Tom Riddle";
+  header_3_text_edad: string = "71 años";
   header_3_text_lugar: string = "Salta Capital";
-  header_3_text_oficio: string = "Carpintero";
+  header_3_text_oficio: string = "Mago";
 
   historias = ["navidad", "vacaciones", "nuevo perrito", "primer trabajo", "navidad", "vacaciones", "nuevo perrito", "primer trabajo"];
 
